ca2fbc64ffd8c99413ba2396c73818d936fa5f64
BronnyPRO <fyodorshchegolev@gmail.com>
2023-10-02 21:16:19 +0700

    Create app in main

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from entrance import Ui_MainWindow
+
+class ServeSmart(QMainWindow):
+    def __init__(self):
+        super(ServeSmart, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ServeSmart()
+    window.show()
+
+    sys.exit(app.exec())
